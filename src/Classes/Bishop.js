0090390f11cd651dccdf81e2acf524cfa47fe122
jkim0617 <110431431+jkim0617@users.noreply.github.com>
2024-10-04 14:38:26 -0700

    Update Bishop.js

--- src/Classes/Bishop.js
+++ src/Classes/Bishop.js
@@ -3,7 +3,7 @@ import Piece from "./Piece";
 class Bishop extends Piece {
   constructor(index, side) {
     super(index, side);
-    this.type = "bishop"
+    this.type = "bishop";
     this.img = (side === "white" ? "https://upload.wikimedia.org/wikipedia/commons/9/9b/Chess_blt60.png" : "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png")
   }
 }
